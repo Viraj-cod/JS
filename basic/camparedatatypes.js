0899c2b2fb2ff59fa7afb93ab0a8bd1497b0b601
Viraj Desai <dviraj812@gmail.com>
2024-10-21 02:23:00 +0000

    compare

---
+++ basic/camparedatatypes.js
@@ -0,0 +1,16 @@
+//console.log(null>0);//false
+//console.log(null == 0); // false
+//console.log(null >= 0);//true
+//console.log(undefined == 0); //comapre with undefinde in any it gives the result as false 
+//console.log('hel');
+
+
+//=== , strict check
+
+console.log(2=='2');//true
+console.log(2==='2');//false
+
+//code should be readable
+
+
+
