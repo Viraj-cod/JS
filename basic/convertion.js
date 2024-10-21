3f0cd3ddb8169f88b4a2cc102c8db4e3a0c4086c
Viraj Desai <dviraj812@gmail.com>
2024-10-21 01:51:27 +0000

    conversions

---
+++ basic/convertion.js
@@ -0,0 +1,21 @@
+let score = '43'
+
+//console.log(typeof(score));
+
+//let valuenumber = Number(score)
+//console.log(typeof valuenumber);
+//let sc = '33wdf'
+//let val = Number(sc)
+//console.log(typeof sc);
+//console.log(val); // op:NaN
+
+//let sl = null
+//let valu = Number(sl)
+//console.log(typeof sl);
+//console.log(valu); // op:0
+
+//undefinde will give undefined
+let num = 33;
+
+let newnum = String(num)
+console.log(typeof newnum);
