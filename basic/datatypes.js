37c1e67889691a44597ad6e16e72677c265abd53
Viraj Desai <dviraj812@gmail.com>
2024-10-21 01:34:30 +0000

    Variable and datatypes

---
+++ basic/datatypes.js
@@ -0,0 +1,22 @@
+"use strict";//treat all js code as newer version
+
+//alert(3+3) // we are using nodejs, not browser
+
+// code readilbility should be high
+
+let name = 'viraj'
+let age = 24
+let siloggedin = true
+let state;
+let number =null;
+//number
+//bigint
+//string
+//boolean
+//null
+//undefined
+//symbol => unique,used in the react
+//objects
+
+console.log(typeof(state));//undefined
+console.log(typeof(number));//object
---
+++ basic/variables.js
@@ -0,0 +1,6 @@
+const number = 12345
+let num = 999
+var nm = 321
+let jai;
+var nm = 769;
+console.table([number,num,nm,jai,nm]);
