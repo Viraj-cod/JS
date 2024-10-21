295622fb87039d6ee9c4ad7a1c68977b1f374915
Viraj Desai <dviraj812@gmail.com>
2024-10-21 02:06:57 +0000

    Operations

---
+++ basic/operations.js
@@ -0,0 +1,24 @@
+let val = 3;
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**3);
+//console.log(2/3);
+//console.log(2%3);
+//console.log(1+2 + "2"); //32
+//console.log("1"+2+2); //122
+ 
+let game = 100;
+++game; //prefix operator ,101
+game++; //postfix operator ,102
+console.log(game);
+//console.log();
+
+
+
+
+
+
+
+
